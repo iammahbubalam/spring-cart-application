e72a91931d8809738dca4bb64e6d89137e0b9fb7
iammahbubalam <bubjamnojrin7196@gmail.com>
2025-03-15 05:27:27 +0530

    init

---
+++ src/main/java/com/iammahbubalam/spring_cart_application/entities/Category.java
@@ -0,0 +1,18 @@
+package com.iammahbubalam.spring_cart_application.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Category {
+    @Id
+    private Long id;
+}
---
+++ src/main/java/com/iammahbubalam/spring_cart_application/entities/Product.java
@@ -0,0 +1,19 @@
+package com.iammahbubalam.spring_cart_application.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Product {
+    @Id
+    private Long id;
+
+}
